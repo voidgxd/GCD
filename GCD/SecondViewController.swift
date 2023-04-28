320c00e00b8dfb6f2f206b044b981dc423260fea
voidgxd <118388964+voidgxd@users.noreply.github.com>
2023-04-28 13:30:48 +0300

    InitialCommit

---
+++ GCD/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  GCD
+//
+//  Created by Максим Мосалёв on 28.04.2023.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
