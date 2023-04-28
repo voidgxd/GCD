e0709b101fb85d66e697e8d886b251ebc0d9e804
voidgxd <118388964+voidgxd@users.noreply.github.com>
2023-04-28 15:11:20 +0300

    Barriers

---
+++ GCD/Barriers.playground/Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+class SafeArray<Element> {
+    private var array: [Element] = []
+    private let queue = DispatchQueue(label: "DispatchBarrier", attributes: .concurrent)
+    
+    public func append(element: Element) {
+        queue.async(flags: .barrier) {
+            self.array.append(element)
+        }
+    }
+    
+    public var elements: [Element] {
+        var result: [Element] = []
+        queue.sync {
+            result = self.array
+        }
+       return result
+    }
+}
+
+
+var safeArray = SafeArray<Int>()
+
+DispatchQueue.concurrentPerform(iterations: 10) { (index) in
+    safeArray.append(element: index)
+}
+
+print("array: \(safeArray.elements)")
+
+var array = [Int]()
+
+DispatchQueue.concurrentPerform(iterations: 10) { (index) in
+    array.append(index)
+}
+
+print("array: \(array)")
--- GCD/SecondViewController.swift
+++ GCD/SecondViewController.swift
@@ -10,7 +10,68 @@ import UIKit
 class SecondViewController: UIViewController {
     
     
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    fileprivate var imageURL: URL?
+    fileprivate var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        
+        set {
+            activityIndicator.stopAnimating()
+            activityIndicator.isHidden = true
+            imageView.image = newValue
+            imageView.sizeToFit()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchImage()
+        delay(3) {
+            self.loginAlert()
+        }
+    }
+    
+    fileprivate func delay(_ delay: Int, closure: @escaping () -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(delay)) {
+            closure()
+        }
+    }
+    
+    fileprivate func loginAlert() {
+        let ac = UIAlertController(title: "Is registered?", message: "Enter your login and pasword", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default)
+        
+        ac.addAction(okAction)
+        ac.addAction(cancelAction)
+        
+        ac.addTextField { (usernameTF) in
+            usernameTF.placeholder = "Enter login"
+        }
+        
+        ac.addTextField { (userPaswordTF) in
+            userPaswordTF.placeholder = "Enter pasword"
+            userPaswordTF.isSecureTextEntry = true
+        }
+        
+        self.present(ac, animated: true, completion: nil)
+    }
+    
+    fileprivate func fetchImage() {
+        imageURL = URL(string: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/LeBron_James_-_51959723161_%28cropped%29.jpg/1024px-LeBron_James_-_51959723161_%28cropped%29.jpg")
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        let queue = DispatchQueue.global(qos: .utility)
+        queue.async {
+            guard let url = self.imageURL, let imageData = try? Data(contentsOf: url) else  { return }
+            DispatchQueue.main.async {
+                self.image = UIImage(data: imageData)
+            }
+        }
     }
 }
